3f337ac30819e19433cc04cc317817247a5a36b8
Dylan Lee <cubeslice@users.noreply.github.com>
2018-07-31 11:47:57 -0400

    organized

--- src/mechanics/Combat.java
+++ src/mechanics/Combat.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 import livingthings.*;
 
 public class Combat {
-	
 	public static boolean beatEnemy() {
 		Scanner keyboard = new Scanner(System.in);
 		int playerHealth = (Player.health);
 		int playerAttack = (Player.attack);
 		int enemyHealth = RNG.randomRange(30, 50);
 		int enemyAttack = RNG.randomRange(25, 35);
-		int chance;
-		combatloop: while(enemyHealth>0 && playerHealth>0) {
+		int chance;	
+		combatloop: while (enemyHealth > 0 && playerHealth > 0) {
 			System.out.println("----------------------------------------------");
 			System.out.println("\tYour Health: " + playerHealth);
 			System.out.println("\tEnemy Health: " + enemyHealth);
@@ -22,57 +21,53 @@ public class Combat {
 			System.out.println("\t2. Heal");
 			System.out.println("\t3. Run!");
 			System.out.println("----------------------------------------------");
-			
 			int opt = keyboard.nextInt();
-			switch(opt) {
+			switch (opt) {
 			case 1:
 				chance = RNG.randomNumber(20);
-				if(chance <= 4) {
+				if (chance <= 4) {
 					System.out.println("You missed!");
-				}
-				else {
-					int realAttack = RNG.randomRange(playerAttack - 10 , playerAttack + 10);
+				} else {
+					int realAttack = RNG.randomRange(playerAttack - 10, playerAttack + 10);
 					enemyHealth -= realAttack;
-					System.out.println("You have hit the enemy for " +realAttack+ " damage.");
+					System.out.println("You have hit the enemy for " + realAttack + " damage.");
 				}
 				break;
-			case 2: 
+			case 2:
 				chance = RNG.randomNumber(20);
-				if(chance <= 2) {
+				if (chance <= 2) {
 					System.out.println("You couldn't heal!");
-				}
-				else {
+				} else {
 					int heal = RNG.randomRange(20, 30);
 					playerHealth += heal;
-					if(playerHealth>100)
+					if (playerHealth > 100)
 						playerHealth = 100;
 					System.out.println(heal + " HP has been restored to your health!");
 				}
 				break;
 			case 3:
 				chance = RNG.randomNumber(20);
-				if(chance == 1) {
+				if (chance == 1) {
 					System.out.println("You have successfully run away!");
-					enemyHealth=0;
+					enemyHealth = 0;
 					break combatloop;
-				}
-				else {
+				} else {
 					System.out.println("You couldn't run away!");
 				}
 				break;
 			default:
 				System.out.println("That is an invalid input. The enemy gets a free hit.");
 			}
-			if(enemyHealth>0) {
+			if (enemyHealth > 0) {
 				int realEnemyAttack = RNG.randomRange(enemyAttack - 5, enemyAttack + 5);
 				playerHealth -= realEnemyAttack;
-				System.out.println("The enemy has hit you for " +realEnemyAttack+ " damage.");
+				System.out.println("The enemy has hit you for " + realEnemyAttack + " damage.");
 			}
 		}
-		if(playerHealth > 0) {
+		if (playerHealth > 0) {
 			System.out.println("You have bested the enemy!");
 			return true;
-			
+
 		} else {
 			System.out.println("You died.");
 			return false;
